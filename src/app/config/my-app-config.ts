a5fe2afc5f034133233c2106cbf30470bd0f464c
Siddharth Dedhia <62965250+SiddharthDedhia@users.noreply.github.com>
2021-06-20 19:46:47 +0200

    Update my-app-config.ts

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,9 +1,11 @@
 export default {
     oidc: {
-        clientId:'0oa118jqtx4BMr0yP5d7',
-        issuer:'https://dev-91013830.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback', //sends user when they login
+        clientId:'clientID',
+        issuer:'issuer',
+        redirectUri:'redirectURI', //sends user when they login
         scopes:['openid','profile','email'] //scopes provide access to info about user
     }
 
 }
+
+//insert clientID,issuer and redirectURI from Okta
